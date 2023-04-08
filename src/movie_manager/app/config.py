26223e66cd07e665958ae959ff93a079d07ca38d
Petr Synek <petr.synek@threatmark.com>
2023-04-08 21:57:18 +0200

    - added endpoint which provides fields which are filterable and their possible values - updated the html to support filtering on those fields - some cleanup of html code

--- src/movie_manager/app/config.py
+++ src/movie_manager/app/config.py
@@ -40,7 +40,7 @@ class MovieManagerSettings(BaseSettings):
 
     mongo_uri: Union[str, MyMongoDsn] = "mongodb://mongo:27017/movie_database"
     remote_api_url: str = "https://gist.githubusercontent.com/nextsux/f6e0327857c88caedd2dab13affb72c1/raw/04441487d90a0a05831835413f5942d58026d321/videos.json"  # noqa: E501
-    remote_api_poll_interval: int = 1
+    remote_api_poll_interval: int = 100
     loglevel: str = "DEBUG"
 
     @validator("mongo_uri", pre=True)
--- src/movie_manager/app/main.py
+++ src/movie_manager/app/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import database_operations
@@ -20,36 +21,64 @@ logging.info(settings)
 app = FastAPI()
 
 # Start the database updater
-movie_fetcher.run(
+movie_fetcher_task = movie_fetcher.run(
     poll_interval=settings.remote_api_poll_interval,
     poll_url=settings.remote_api_url,
     collection=movies,
 )
 
 
+@app.on_event("shutdown")
+def shutdown_event():
+    """Close the database connection and correctly terminate task on shutdown."""
+    movie_fetcher_task.cancel()
+    connection.close()
+
+
 @app.get("/")
 def read_root():
-    """Serve the frontend page."""
+    """Serve the frontend page, should be done by nginx in production."""
+
     with open("frontend/page.html", "r") as f:
         html_content = f.read()
+
     return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/api/search")
-def name_search(search_term: str, order: bool = True):
+def name_search(search_term: str, filters: str, order: bool = True):
     """Search for movies by name."""
 
+    # set ordering
     ordering = pymongo.ASCENDING if order else pymongo.DESCENDING
 
-    if not search_term:
-        data = movies.find({}).sort("name", ordering)
-    else:
-        data = movies.find({"name": {"$regex": search_term, "$options": "i"}}).sort(
-            "name", ordering
-        )
+    # build filter query
+    parsed_filters = json.loads(filters)
 
-    repsonse = []
-    for movie in data:
-        image = movie.get("iconUri", None)
-        repsonse.append({"name": movie["name"], "image": image})
-    return repsonse
+    filter_query = {key: {"$in": parsed_filters[key]} for key in parsed_filters}
+
+    # search querry
+    if not search_term:
+        find_querry = {}
+    else:
+        find_querry = {"name": {"$regex": search_term, "$options": "i"}}
+
+    # final querry
+    querry = {**find_querry, **filter_query}
+
+    # fetch data
+    data = movies.find(querry).sort("name", ordering)
+
+    return [
+        {"name": movie["name"], "image": movie.get("iconUri", None)} for movie in data
+    ]
+
+
+@app.get("/api/searchfields")
+def provide_filters():
+    """Returns dict of unique values for selected fields in mongoDB collection."""
+
+    # fields which I want to filter by
+    fields = ["features", "source", "drm"]
+
+    return {field: movies.distinct(field) for field in fields}
